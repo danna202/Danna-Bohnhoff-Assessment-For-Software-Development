c3953b8d77d6e143b13fd9cc99408cfbb43052ad
danna202 <105758399+danna202@users.noreply.github.com>
2022-06-14 23:58:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Obtain the img element, and assign it to the image variable
+let pet = document.getElementById('#pet')
+
+
+
+// Obtain the 'shrink-grow' button, and assign it to the changeSize variable
+let changeSize = document.getElementById('#"shrink-grow">Shrink/Grow<
+
+                                                                                     //DO NOT CHANGE THE FOLLOWING CODE
+changeSize.addEventListener("click", function() {
+  if (image.style.height == "100px") {
+    image.style.height = "25px"
+  }
+  else {
+    image.style.height = "100px"
+  }
+})
